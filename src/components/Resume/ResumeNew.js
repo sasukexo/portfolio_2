c10624fd60213f5aa9e8729bfded36365a351e02
Sasukexo <aswinash1002@gmail.com>
2023-08-14 23:57:06 +0530

    added resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
+// import pdf from "../../Assets/../Assets/Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import ResumePDF from "../../Assets/../Assets/Resume.pdf"; // Import the new PDF file
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+const resumeLink =   "https://raw.githubusercontent.com/sasukexo/portfolio_2/master/src/Assets/Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -22,7 +23,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={ResumePDF} // Use the new imported PDF file
+            href={resumeLink}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
@@ -32,7 +33,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={ResumePDF} className="d-flex justify-content-center">
+          <Document file={resumeLink} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
@@ -40,7 +41,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={ResumePDF} // Use the new imported PDF file
+            href={resumeLink}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
