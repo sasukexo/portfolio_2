337050538dbe2547f910ab8dac707c05e850874c
Sasukexo <aswinash1002@gmail.com>
2023-08-17 20:26:02 +0530

    changed resume path

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -7,7 +7,7 @@ import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-const resumeLink =   "https://raw.githubusercontent.com/sasukexo/portfolio_2/master/src/Assets/Resume.pdf"
+const resumeLink =   "https://raw.githubusercontent.com/sasukexo/portfolio_2/master/src/Assets/Resume-final.pdf"
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
